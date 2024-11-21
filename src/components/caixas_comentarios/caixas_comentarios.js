6b3975ce9fc7c30a9c6a775acccd54cc5b0bb63f
Paulo Igor Torres <pauloigortorres@hotmail.com>
2024-11-21 20:59:12 -0300

    acrescimo de metodo map

--- src/components/caixas_comentarios/caixas_comentarios.js
+++ src/components/caixas_comentarios/caixas_comentarios.js
@@ -5,54 +5,46 @@ import Avatar3 from "../../assets/images (1).jpg";
 import Avatar4 from "../../assets/photo-1606893995103-a431bce9c219.jpg";
 import './caixas_comentarios.css';
 
+let comentarios = [
+    {
+        avatar: Avatar1,
+        nome: "Roberto Almeida",
+        comentario: '"Acho que isso não tem nada a ver com a Mera, porque ela quase não aparece no filme.”'
+    },
+    {
+        avatar: Avatar2,
+        nome: "Juliana Tavares",
+        comentario: '“Não tenho nenhum prazer em revelar que os testes de audiência de Aquaman 2 não foram positivos. Espero que o James Wan consiga consertar esse filme até dezembro. Ouvi dizer que algo que quase nunca acontece durante testes de exibição aconteceu com esse filme,”'
+    },
+    {
+        avatar: Avatar3,
+        nome: "Paulo Oliveira",
+        comentario: '"Estou ansioso pelo filme!"'
+    },
+    {
+        avatar: Avatar4,
+        nome: "Amanda Gomes",
+        comentario: '“Há uma razão pela qual tenho evitado qualquer pergunta sobre AQUAMAN E O REINO PERDIDO, mas desde que o ViewerAnon divulgou, não me traz alegria apoiá-lo e dizer que as exibições de teste para este filme não foram boas. Realmente esperando que Wan corrija este filme antes de dezembro.”'
+    }
+];
+
 function CaixasComentarios() {
 
     return (
         <main id="main_comentarios">
-            <div className="caixa_comentarios">
-                <div className="avatar">
-                    <img src={Avatar1} alt="A imagem não pode ser carregada" className="retrato"/>
-                    <span className="nome">Roberto Almeida</span>
+            {comentarios.map((item) => (
+                <div className="caixa_comentarios">
+                    <div className="avatar">
+                        <img src={item.avatar} alt="A imagem não pode ser carregada" className="retrato"/>
+                        <span className="nome">{item.nome}</span>
+                    </div>
+                    <div className="comentario">
+                        <p>
+                            {item.comentario}
+                        </p>
+                    </div>
                 </div>
-                <div className="comentario">
-                    <p>
-                        "Acho que isso não tem nada a ver com a Mera, porque ela quase não aparece no filme.”
-                    </p>
-                </div>
-            </div>
-            <div className="caixa_comentarios">
-                <div className="avatar">
-                    <img src={Avatar2} alt="A imagem não pode ser carregada" className="retrato"/>
-                    <span className="nome">Juliana Tavares</span>
-                </div>
-                <div className="comentario">
-                    <p>
-                        “Não tenho nenhum prazer em revelar que os testes de audiência de Aquaman 2 não foram positivos. Espero que o James Wan consiga consertar esse filme até dezembro. Ouvi dizer que algo que quase nunca acontece durante testes de exibição aconteceu com esse filme,”
-                    </p>
-                </div>
-            </div>
-            <div className="caixa_comentarios">
-                <div className="avatar">
-                    <img src={Avatar3} alt="A imagem não pode ser carregada" className="retrato"/>
-                    <span className="nome">Paulo Oliveira</span>
-                </div>
-                <div className="comentario">
-                    <p>
-                        "Estou ansioso pelo filme!"
-                    </p>
-                </div>
-            </div>
-            <div className="caixa_comentarios">
-                <div className="avatar">
-                    <img src={Avatar4} alt="A imagem não pode ser carregada" className="retrato"/>
-                    <span className="nome">Amanda Gomes</span>
-                </div>
-                <div className="comentario">
-                    <p>
-                        “Há uma razão pela qual tenho evitado qualquer pergunta sobre AQUAMAN E O REINO PERDIDO, mas desde que o ViewerAnon divulgou, não me traz alegria apoiá-lo e dizer que as exibições de teste para este filme não foram boas. Realmente esperando que Wan corrija este filme antes de dezembro.”
-                    </p>
-                </div>
-            </div>
+            ))}
         </main>
     )
 };
